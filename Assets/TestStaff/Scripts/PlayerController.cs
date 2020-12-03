497c1662820ad577d38c98f13c11147f14a1ae74
Filip <filipbanaszak333@gmail.com>
2020-12-03 14:24:00 +0100

    Add test staff

---
+++ Assets/TestStaff/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float maxSpeed;
+	[SerializeField] private float speed;
+
+	private Rigidbody2D Rb { get; set; }
+	private float Direction { get; set; }
+
+	private void Start()
+	{
+		Rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+    {
+        Direction = Input.GetAxis("Horizontal");
+    }
+
+	private void FixedUpdate()
+	{
+		Rb.velocity = new Vector2(Direction * speed, 0);
+		Rb.velocity = Vector2.ClampMagnitude(Rb.velocity, maxSpeed);
+	}
+}
