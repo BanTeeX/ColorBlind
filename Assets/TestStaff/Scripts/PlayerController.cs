931b0a383924abc2f963f9992a0727632ab19dcb
Filip <filipbanaszak333@gmail.com>
2020-12-18 05:25:16 +0100

    Add horizontal move

--- Assets/TestStaff/Scripts/PlayerController.cs
+++ Assets/TestStaff/Scripts/PlayerController.cs
@@ -3,7 +3,9 @@
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private float maxSpeed;
-	[SerializeField] private float speed;
+	[SerializeField] private float speedToleration;
+	[SerializeField] private float time;
+	[SerializeField] private float slowingTime;
 
 	private Rigidbody2D Rb { get; set; }
 	private float Direction { get; set; }
@@ -15,12 +17,23 @@ public class PlayerController : MonoBehaviour
 
 	private void Update()
     {
-        Direction = Input.GetAxis("Horizontal");
+        Direction = Input.GetAxisRaw("Horizontal");
     }
 
 	private void FixedUpdate()
 	{
-		Rb.velocity = new Vector2(Direction * speed, 0);
-		Rb.velocity = Vector2.ClampMagnitude(Rb.velocity, maxSpeed);
+		if (Direction != 0)
+		{
+			Rb.AddForce(new Vector2(Rb.mass * (maxSpeed / time), 0) * Direction);
+		}
+		else
+		{
+			Rb.AddForce(new Vector2(-Rb.mass * (Rb.velocity.x / slowingTime), 0));
+		}
+		Rb.velocity = new Vector2(Mathf.Clamp(Rb.velocity.x, -maxSpeed, maxSpeed), Rb.velocity.y);
+		if (Mathf.Abs(Rb.velocity.x) < speedToleration)
+		{
+			Rb.velocity = new Vector2(0, Rb.velocity.y);
+		}
 	}
 }
