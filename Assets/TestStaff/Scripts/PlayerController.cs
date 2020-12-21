a1e0045d5a0d393ace402bd21a9bbcba970d6de4
Filip <filipbanaszak333@gmail.com>
2020-12-21 15:57:27 +0100

    Update vertical movement

--- Assets/TestStaff/Scripts/PlayerController.cs
+++ Assets/TestStaff/Scripts/PlayerController.cs
@@ -4,36 +4,61 @@ public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private float maxSpeed;
 	[SerializeField] private float speedToleration;
-	[SerializeField] private float time;
+	[SerializeField] private float accelerationTime;
 	[SerializeField] private float slowingTime;
+	[SerializeField] private float maxJumpTime;
+	[SerializeField] private float groundCheckRadius;
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private Rigidbody2D rb;
 
-	private Rigidbody2D Rb { get; set; }
 	private float Direction { get; set; }
-
-	private void Start()
-	{
-		Rb = GetComponent<Rigidbody2D>();
-	}
+	private bool Jump { get; set; }
+	private bool JumpFirstFrame { get; set; } = true;
+	private bool IsGrounded { get; set; }
 
 	private void Update()
     {
         Direction = Input.GetAxisRaw("Horizontal");
+		if (IsGrounded || Jump)
+		{
+			Jump = Input.GetButton("Jump");
+		}
     }
 
 	private void FixedUpdate()
 	{
+		IsGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, LayerMask.GetMask("Black", "White"));
+
+		if (IsGrounded)
+		{
+			JumpFirstFrame = true;
+		}
+
+		rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
+		if (Mathf.Abs(rb.velocity.x) < speedToleration)
+		{
+			rb.velocity = new Vector2(0, rb.velocity.y);
+		}
 		if (Direction != 0)
 		{
-			Rb.AddForce(new Vector2(Rb.mass * (maxSpeed / time), 0) * Direction);
+			rb.AddForce(new Vector2(rb.mass * (maxSpeed / accelerationTime), 0) * Direction);
 		}
 		else
 		{
-			Rb.AddForce(new Vector2(-Rb.mass * (Rb.velocity.x / slowingTime), 0));
+			rb.AddForce(new Vector2(-rb.mass * (rb.velocity.x / slowingTime), 0));
 		}
-		Rb.velocity = new Vector2(Mathf.Clamp(Rb.velocity.x, -maxSpeed, maxSpeed), Rb.velocity.y);
-		if (Mathf.Abs(Rb.velocity.x) < speedToleration)
+
+		if (Jump)
 		{
-			Rb.velocity = new Vector2(0, Rb.velocity.y);
+			if (JumpFirstFrame)
+			{
+				rb.AddForce(new Vector2(0, 500));
+				JumpFirstFrame = false;
+			}
+			else
+			{
+				rb.AddForce(new Vector2(0, -Physics2D.gravity.y * rb.mass * rb.gravityScale));
+			}
 		}
 	}
 }
